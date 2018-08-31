e5bc6d7b5d51d52929f1fde0f66eff1cdc361714
wangtianyu <wangtianyuwangtianyu@didichuxing.com>
2018-08-31 10:55:02 +0800

    feat: add w3-4

--- week3/w3-4.js
+++ week3/w3-4.js
@@ -8,12 +8,22 @@ floor_power_of2(32) // 32
  */
 
 //第一种
-var floor_power_of2 = (num) => {
-  let powNum=0;
-  for(let i=1; Math.pow(2,i)<=num; i++) {
-    powNum = Math.pow(2,i);
+const floor_power_of2 = (num) => {
+  let i=0;
+  while(num >= 2) {
+    num = num/2;
+    ++i;
   }
-  return powNum;
+  return Math.pow(2,i);
+}
+console.log(floor_power_of2(64));
+console.log(floor_power_of2(63));
+console.log(floor_power_of2(32));
+console.log(floor_power_of2(33));
+
+//第二种
+const floor_power_of2 = (num) => {
+  return Math.pow(2,~~Math.log2(num));
 }
 console.log(floor_power_of2(64));
 console.log(floor_power_of2(63));
